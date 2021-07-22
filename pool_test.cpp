dbaf4fd1bd91807d489b40ce252790770d64d20f
Guanjun Li <39039919+ligj123@users.noreply.github.com>
2021-07-22 18:03:06 +0800

    Update pool_test.cpp

--- pool_test.cpp
+++ pool_test.cpp
@@ -1,12 +1,24 @@
-﻿#include "ThreadPool.h"
+#include "ThreadPool.h"
 #include <iostream>
 
 using namespace std;
 using namespace utils;
 
+class A {
+public:
+  void test_task(int para) {
+    // std::cout << "start task; para=" << para << std::endl;
+  }
+};
+
 int main() {
+  A a;
   ThreadPool tp("Test_ThreadPool", 1000);
-  std::cout << "thread num1: " << tp.GetThreadCount() << std::endl;
+  this_thread::sleep_for(chrono::milliseconds(1));
+  tp.AddTask(&A::test_task, &a, 100);
+
+  std::cout << "thread num1: " << tp.GetThreadCount()
+            << "  free:" << tp.GetFreeThreads() << std::endl;
   bool bStop = false;
   for (int i = 0; i < 20; i++) {
     tp.AddTask([&bStop]() {
@@ -17,11 +29,13 @@ int main() {
   }
 
   this_thread::sleep_for(chrono::milliseconds(10));
-  std::cout << "thread num2: " << tp.GetThreadCount() << std::endl;
+  std::cout << "thread num2: " << tp.GetThreadCount()
+            << "  free:" << tp.GetFreeThreads() << std::endl;
   bStop = true;
   this_thread::sleep_for(chrono::milliseconds(1000));
   std::cout << "thread num3: " << tp.GetThreadCount()
-            << "   CurrId: " << tp.GetCurrId() << std::endl;
+            << "   CurrId: " << tp.GetCurrId()
+            << "  free:" << tp.GetFreeThreads() << std::endl;
 
   bStop = false;
   for (int i = 0; i < 20; i++) {
@@ -33,12 +47,14 @@ int main() {
   }
 
   this_thread::sleep_for(chrono::milliseconds(10));
-  std::cout << "thread num4: " << tp.GetThreadCount() << std::endl;
+  std::cout << "thread num4: " << tp.GetThreadCount()
+            << "  free:" << tp.GetFreeThreads() << std::endl;
   bStop = true;
 
   this_thread::sleep_for(chrono::milliseconds(1000));
   std::cout << "thread num5: " << tp.GetThreadCount()
-            << "   CurrId: " << tp.GetCurrId() << std::endl;
+            << "   CurrId: " << tp.GetCurrId()
+            << "  free:" << tp.GetFreeThreads() << std::endl;
 
   return 0;
 }
